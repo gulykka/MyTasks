42fb98f323336e6e1968a00ab814545f948531ea
gulykka <103120773+gulykka@users.noreply.github.com>
2023-01-29 04:22:12 +1000

    add Models

--- app/src/main/java/com/example/myapplication/models/ListModel.kt
+++ app/src/main/java/com/example/myapplication/models/ListModel.kt
@@ -1,4 +1,16 @@
 package com.example.myapplication.models
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "list_table")
 class ListModel {
+    @PrimaryKey(autoGenerate = true)
+    var id_list: Int = 0
+
+    @ColumnInfo
+    var title_list: String = ""
+
 }
--- app/src/main/java/com/example/myapplication/models/TaskModel.kt
+++ app/src/main/java/com/example/myapplication/models/TaskModel.kt
@@ -1,4 +1,27 @@
 package com.example.myapplication.models
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "task_table")
 class TaskModel {
+    @PrimaryKey(autoGenerate = true)
+    var id_task: Int = 0
+
+    @ColumnInfo
+    var title_task: String = ""
+
+    @ColumnInfo
+    var data_create: Long = 0
+
+    @ColumnInfo
+    var description: String = ""
+
+    @ColumnInfo
+    var is_performed: Boolean = true
+
+
+    @ColumnInfo
+    var number_list: Int = 0
 }
