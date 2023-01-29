82b606da5c75114cda28ade133809fed15997a6f
gulykka <103120773+gulykka@users.noreply.github.com>
2023-01-30 07:51:07 +1000

    adding new tasks changing data in the detail view deleting tasks in the detail view

--- app/src/main/java/com/example/myapplication/screens/detail/DetailFragment.kt
+++ app/src/main/java/com/example/myapplication/screens/detail/DetailFragment.kt
@@ -1,20 +1,62 @@
 package com.example.myapplication.screens.detail
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.example.myapplication.APP
 import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentDetailBinding
+import com.example.myapplication.models.TaskModel
+
 
 class DetailFragment : Fragment() {
+
+    lateinit var binding: FragmentDetailBinding
+    lateinit var currentTask: TaskModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        binding = FragmentDetailBinding.inflate(layoutInflater, container, false)
+        currentTask = arguments?.getSerializable("task") as TaskModel
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+
+        val viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+
+        binding.titleTask.setText(currentTask.title_task)
+        binding.descriptionTask.setText(currentTask.description)
+        binding.checkBoxTask.isChecked = currentTask.chosen
+
+        binding.buttonDeleteTask.setOnClickListener {
+            viewModel.delete(currentTask){}
+
+            APP.navController.navigate(R.id.action_detailFragment_to_startFragment)
+        }
+
+        binding.buttonBack.setOnClickListener{
+            val title_new = binding.titleTask.getText().toString()
+            val description_new = binding.descriptionTask.getText().toString()
+            val chosen_new = binding.checkBoxTask.isChecked
+            val updateTask = TaskModel(currentTask.id_task, title_task = title_new, description = description_new, chosen = chosen_new)
+            viewModel.update(updateTask){}
+            APP.navController.navigate(R.id.action_detailFragment_to_startFragment)
+        }
+
+
+
     }
 
 }
--- app/src/main/java/com/example/myapplication/screens/detail/DetailViewModel.kt
+++ app/src/main/java/com/example/myapplication/screens/detail/DetailViewModel.kt
@@ -1,6 +1,25 @@
 package com.example.myapplication.screens.detail
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myapplication.REPOSITORY
+import com.example.myapplication.models.TaskModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class DetailViewModel:ViewModel() {
+    fun delete(taskModel: TaskModel, onSuccess:() -> Unit) =
+        viewModelScope.launch (Dispatchers.IO ) {
+            REPOSITORY.deleteTask(taskModel){
+                onSuccess()
+            }
+        }
+
+    fun update(taskModel: TaskModel, onSuccess:() -> Unit) =
+        viewModelScope.launch (Dispatchers.IO ) {
+            REPOSITORY.updateTask(taskModel){
+                onSuccess()
+            }
+        }
+
 }
